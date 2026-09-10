5ecbfcb86ccff0c738b0de95bb6cfa8d5736282a
Lalla Soubai <lasoubai@c1r7p14.1337.ma>
2026-09-10 17:32:02 +0100

    fist working version-1

--- src/mariadb/tools/mariadb.sh
+++ src/mariadb/tools/mariadb.sh
@@ -1,42 +1,28 @@
 #!/bin/bash
 
-
-service mariadb start  
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\` ;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%'  IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%';" 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
-
-# exec mysqld_safe
-exec bash
-
-
-
-# # #!/bin/bash
-
 # # Ensure socket directory exists on runtime
-# mkdir -p /run/mysqld
-# chown -R mysql:mysql /run/mysqld
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
 
-# # Check if database setup was already initialized
-# if [ ! -d "/var/lib/mysql/${SQL_DATABASE}" ]; then
 
-#     service mariadb start  
 
-#     until mysqladmin ping >/dev/null 2>&1; do
-#         sleep 1
-#     done
+# sleep 3
+if [ ! -d "/var/lib/mysql/${MARIADB_DATABASE}" ]; then
 
-#     mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-#     mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-#     mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%';" 
-#     mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-#     mysql -e "FLUSH PRIVILEGES;"
+service mariadb start 
 
-#     mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
-# fi
+until mysqladmin ping >/dev/null 2>&1; do
+        sleep 1
+    done
 
-# # Launch PID 1 process
-# exec mysqld_safe
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\` ;"
+mysql -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'%'  IDENTIFIED BY '${MARIADB_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO '${MARIADB_USER}'@'%';" 
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
+# mysql -e "FLUSH PRIVILEGES;"
+mysql -u root -p"${MARIADB_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p"${MARIADB_ROOT_PASSWORD}" shutdown
+
+fi
+
+exec mysqld_safe
--- src/wordpress/tools/wordpress.sh
+++ src/wordpress/tools/wordpress.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+# socket directory exists on runtime?
+echo "Waiting for MariaDB..."
+sleep 15
 
-sleep 10
+
 mkdir -p  /var/www/wordpress
 cd /var/www/wordpress
 
 wp core download --allow-root
 
-wp core config --dbname=$DOMAINE_NAME --dbuser=$SQL_USER \
-    --dbpass=$SQL_PASSWORD --dbhost=$SQL_HOST  --allow-root --skip-check
+wp core config --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER \
+    --dbpass=$MARIADB_PASSWORD --dbhost=$MARIADB_HOST  --allow-root --skip-check
 
-wp core install --url=$$DOMAINE_NAME --title=$SITE_TITLE --admin_user=$WP_ADMINE_USER \
-    --admine_email=$WP_ADMINE_NAME --admin_password=$WP_ADMIN_PASSWORD --allow-root
+wp core install --url="https://${DOMAINE_NAME}:8099" --title=$SITE_TITLE --admin_user=$WP_ADMINE_USER \
+    --admin_email=$WP_ADMINE_NAME --admin_password=$WP_ADMIN_PASSWORD --allow-root
 
 wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD \
     --role=author --allow-root
 
-# /usr/sbin/php-fpm -F
-exec bash
+exec php-fpm8.2 -F
